e8046bd830edb75831e016af6774d5c9b9ecf15d
sploschee <19555355+sploschee@users.noreply.github.com>
2022-01-03 22:46:34 +0000

    dynamically update index

--- src/main/java/com/splosch/servicea/controller/BitcoinPricingController.java
+++ src/main/java/com/splosch/servicea/controller/BitcoinPricingController.java
@@ -1,16 +1,17 @@
 package com.splosch.servicea.controller;
 
+import java.time.Duration;
+
 import com.splosch.servicea.model.SpotPrice;
 import com.splosch.servicea.service.BitcoinPricingService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
-import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Controller
@@ -19,12 +20,16 @@ public class BitcoinPricingController {
     @Autowired
     private BitcoinPricingService bitcoinPricingService;
 
+    @GetMapping(value = "/spotprices", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ResponseBody
+    Flux<SpotPrice> getSpotPrice(
+            @RequestParam(name = "currency", required = false, defaultValue = "USD") String currency) {
+        return bitcoinPricingService.getSpotPrice(currency).delayElements(Duration.ofSeconds(5));
+
+    }
+
     @GetMapping("/")
-    public String spotPrice(
-            @RequestParam(name = "currency", required = false, defaultValue = "USD") String currency, Model model) {
-        Mono<SpotPrice> spotPriceMono = bitcoinPricingService.getSpotPrice(currency);
-        IReactiveDataDriverContextVariable spotPrice = new ReactiveDataDriverContextVariable(spotPriceMono.flux(), 1);
-        model.addAttribute("prices", spotPrice);
-        return "index";
+    Mono<String> home() {
+        return Mono.just("index");
     }
 }
--- src/main/java/com/splosch/servicea/service/BitcoinPricingService.java
+++ src/main/java/com/splosch/servicea/service/BitcoinPricingService.java
@@ -3,7 +3,8 @@ package com.splosch.servicea.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.splosch.servicea.model.SpotPrice;
-import reactor.core.publisher.Mono;
+
+import reactor.core.publisher.Flux;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 /**
@@ -14,11 +15,11 @@ public class BitcoinPricingService {
 
     private WebClient client = WebClient.create("https://api.coinbase.com/v2");
 
-    public Mono<SpotPrice> getSpotPrice(String currencyCode) {
+    public Flux<SpotPrice> getSpotPrice(String currencyCode) {
         return client.get()
                 .uri("/prices/spot?currency={currencyCode}", currencyCode).accept(APPLICATION_JSON)
                 .retrieve()
-                .bodyToMono(SpotPrice.class);
+                .bodyToFlux(SpotPrice.class);
     }
 
 }
