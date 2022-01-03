a2a4a2e34f1aac967515f55a24a5ceae16e9ef11
sploschee <19555355+sploschee@users.noreply.github.com>
2022-01-03 21:59:13 +0000

    make index

--- src/main/java/com/splosch/servicea/controller/BitcoinPricingController.java
+++ src/main/java/com/splosch/servicea/controller/BitcoinPricingController.java
@@ -1,5 +1,6 @@
 package com.splosch.servicea.controller;
 
+import com.splosch.servicea.model.SpotPrice;
 import com.splosch.servicea.service.BitcoinPricingService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+import reactor.core.publisher.Mono;
 
 @Controller
 public class BitcoinPricingController {
@@ -14,11 +19,12 @@ public class BitcoinPricingController {
     @Autowired
     private BitcoinPricingService bitcoinPricingService;
 
-    @GetMapping("/spotprice")
+    @GetMapping("/")
     public String spotPrice(
             @RequestParam(name = "currency", required = false, defaultValue = "USD") String currency, Model model) {
-        bitcoinPricingService.getSpotPrice(currency);
-        model.addAttribute("currency", currency);
-        return "spotprice";
+        Mono<SpotPrice> spotPriceMono = bitcoinPricingService.getSpotPrice(currency);
+        IReactiveDataDriverContextVariable spotPrice = new ReactiveDataDriverContextVariable(spotPriceMono.flux(), 1);
+        model.addAttribute("prices", spotPrice);
+        return "index";
     }
 }
